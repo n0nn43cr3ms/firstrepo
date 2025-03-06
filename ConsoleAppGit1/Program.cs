f9631e10e175191fe6d84f8669c98fa76b681058
n0nn43cr3ms <igor0914@me.com>
2025-03-06 18:42:27 +0300

    test1
    my first test

---
+++ ConsoleAppGit1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Pipelines;
+using System.Reflection.Metadata;
+
+/*Console.WriteLine("Порядок чисел Фибоначчи");
+Console.Write("Введите n: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());          //Число n
+
+do {
+    FirstNumber--;
+double First = Math.Pow(((1 + Math.Sqrt(5))/2),FirstNumber);
+double Second = Math.Pow(((1 - Math.Sqrt(5))/2),FirstNumber);
+double Result = (First - Second) / Math.Sqrt(5);
+int a = (int) Result;
+Console.WriteLine(a);
+}
+while (FirstNumber>0); */
+
+Console.WriteLine("Порядок чисел Фибоначчи");
+Console.Write("Введите n: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());          //Число n
